bdc220891bdfb76d6a9d2fd29b9ec0331dd11a11
marcelom97 <marcelomollaj@gmail.com>
2020-12-26 17:36:36 +0200

    feat: auth context provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ProvideAuth } from './hooks/use-auth';
 
 import NavBar from './components/NavBar';
 
@@ -12,24 +13,26 @@ import './App.css';
 
 export default function App() {
   return (
-    <Router>
-      <Container>
-        <NavBar />
-        <Switch>
-          <Route path='/' exact>
-            <LandingPage />
-          </Route>
-          <Route path='/menu' exact>
-            <Menu />
-          </Route>
-          <Route path='/signin' exact>
-            <SignIn />
-          </Route>
-          <Route path='/joinnow' exact>
-            <JoinNow />
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Container>
+          <NavBar />
+          <Switch>
+            <Route path='/' exact>
+              <LandingPage />
+            </Route>
+            <Route path='/menu' exact>
+              <Menu />
+            </Route>
+            <Route path='/signin' exact>
+              <SignIn />
+            </Route>
+            <Route path='/joinnow' exact>
+              <JoinNow />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </ProvideAuth>
   );
 }
