df7ba9ab1fad1a42df88b1e6c80133b26b5b1d30
marcelom97 <marcelomollaj@gmail.com>
2020-12-26 19:21:49 +0200

    refactor: added more landing page content

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Row, Image } from 'react-bootstrap';
+import { Row, Image, Button } from 'react-bootstrap';
 
 import Mocha from '../assets/mocha.png';
+import Machiatto from '../assets/machiatto.png';
 
 export default function LandingPage() {
   return (
@@ -12,12 +13,30 @@ export default function LandingPage() {
       <Row className={'d-flex justify-content-between'}>
         <Image src={Mocha} width={'382px'} height={'682px'} />
         <div className={'d-flex align-items-end'}>
-          <div className={'pb-5'} style={{ width: '270px' }}>
+          <div className={'pb-5'} style={{ width: '390px' }}>
             <h1 className={'pb-5 intro-text'}>Enjoy your coffee the way you want it.</h1>
           </div>
         </div>
       </Row>
-      <Row></Row>
+      <Row className={'d-flex flex-row-reverse'}>
+        <div className={'pb-5'} style={{ width: '390px' }}>
+          <h1 className={'pb-5 intro-text'}> Apply for a discount voucher and enjoy more coffee with fewer cost.</h1>
+        </div>
+      </Row>
+      <Row className={'d-flex justify-content-between'}>
+        <div style={{ width: '470px' }}>
+          <h1 className={'intro-text'}>
+            Create an account and apply with your unemployment card to redeem 20% discount on every product.
+          </h1>
+          <br />
+          <div className='d-flex flex-row-reverse'>
+            <Button variant='primary' size='lg' style={{ backgroundColor: '#046240', border: 'none' }}>
+              Join Now
+            </Button>
+          </div>
+        </div>
+        <Image src={Machiatto} width={'657px'} height={'875px'} />
+      </Row>
     </>
   );
 }
