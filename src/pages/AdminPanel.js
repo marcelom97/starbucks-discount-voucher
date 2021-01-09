dc52edaf87675908a2c80f4649300264f064a9c2
marcelom97 <marcelomollaj@gmail.com>
2021-01-09 14:22:50 +0200

    feat: added authContext at the admin panel page

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import { AuthContext } from '../context';
+import { useHistory } from 'react-router-dom';
 
 export default function AdminPanel() {
+  const authContext = React.useContext(AuthContext);
+  const history = useHistory();
+
+  React.useEffect(() => {
+    console.log(authContext);
+    if (!authContext.loggedIn) {
+      history.push('/admin');
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Col>
       <Col className={'d-flex justify-content-center'}>
