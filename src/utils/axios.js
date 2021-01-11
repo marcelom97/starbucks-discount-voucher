73deae1b1193bc6304896b5749bb69d7379778d8
marcelom97 <marcelomollaj@gmail.com>
2021-01-11 16:08:15 +0200

    feat: added starbucks api url to axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const instance = axios.create({ baseURL: process.env.REACT_APP_BACKEND_API_URL });
+const instance = axios.create({ baseURL: 'https://marcelo-starbucks-api.herokuapp.com' });
 
 export default instance;
